244ea5c3bd832e96d2710a5cd0e69d7409833567
njeans <nj99@cornell.edu>
2019-10-30 21:04:14 -0500

    Use named volumes for docker containers

--- fabric-test/honeybadgermpc/readme.md
+++ fabric-test/honeybadgermpc/readme.md
@@ -1,3 +1,36 @@
 Build peer and peer docker images and run install and instantiate rockpaperscissors chaincode
 
-```docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v ${PWD}/setup.sh:/opt/setup.sh -w /opt hyperledger/fabric-baseimage:amd64-0.4.14 sh setup.sh```
+```
+docker volume create orderer.example.com-crypto 
+docker volume create peer0.org1.example.com-crypto 
+docker volume create peer0.org2.example.com-crypto 
+docker volume create peer1.org1.example.com-crypto 
+docker volume create peer1.org2.example.com-crypto 
+docker volume create honeybadgerscc 
+docker volume create chaincode 
+docker volume create cli-config
+docker volume create peer-bin
+docker run -it \
+	-v /var/run/docker.sock:/var/run/docker.sock \
+	-v ${PWD}/setup.sh:/opt/setup.sh \
+	-v ${PWD}/docker-compose-from-docker.yaml:/opt/docker-compose-from-docker.yaml \
+	-v peer-bin:/opt/peer-bin \
+	-v orderer.example.com-crypto:/opt/crypto/orderer.example.com \
+	-v peer0.org1.example.com-crypto:/opt/crypto/peer0.org1.example.com \
+	-v peer1.org1.example.com-crypto:/opt/crypto/peer1.org1.example.com \
+	-v peer0.org2.example.com-crypto:/opt/crypto/peer0.org2.example.com \
+	-v peer1.org2.example.com-crypto:/opt/crypto/peer1.org2.example.com \
+	-v honeybadgerscc:/opt/gopath/src/github.com/njeans/honeybadgerscc \
+	-v chaincode:/opt/chaincode \
+	-v cli-config:/opt/crypto/cli \
+	--name setup-honeybadger \
+	-w /opt hyperledger/fabric-baseimage:amd64-0.4.14 \
+	bash setup.sh
+```
+
+Cleanup
+
+```
+docker container rm setup-honeybader
+docker volume rm orderer.example.com-crypto peer0.org1.example.com-crypto peer0.org2.example.com-crypto peer1.org1.example.com-crypto peer1.org2.example.com-crypto honeybadgerscc chaincode cli-config peer-bin
+```
--- fabric-test/honeybadgermpc/setup.sh
+++ fabric-test/honeybadgermpc/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 #dependencies
 apt-get update && apt-get install -y --no-install-recommends \
@@ -30,10 +29,28 @@ go get github.com/syndtr/goleveldb/leveldb
 mkdir -p $GOPATH/src/github.com/njeans/
 cd $GOPATH/src/github.com/njeans/
 git clone https://github.com/njeans/honeybadgerscc.git
+if [ $? -ne 0 ]
+then
+  cd honeybadgerscc
+  git pull 
+  cd ..
+fi
 git clone https://github.com/njeans/fabric-test.git
+if [ $? -ne 0 ]
+then
+  cd fabric-test
+  git pull 
+  cd ..
+fi
 mkdir -p $GOPATH/src/github.com/hyperledger/
 cd $GOPATH/src/github.com/hyperledger/
 git clone https://github.com/njeans/fabric.git
+if [ $? -ne 0 ]
+then
+  cd fabric
+  git pull 
+  cd ..
+fi
 
 #build system chaincode
 cd $GOPATH/src/github.com/njeans/honeybadgerscc/
@@ -44,10 +61,28 @@ export DOCKER_DYNAMIC_LINK=true
 export GO_TAGS="$GO_TAGS pluginsenabled"
 cd $GOPATH/src/github.com/hyperledger/fabric
 make peer
+
 #build fabric peer docker image
 make peer-docker IN_DOCKER=true
 
-#install and instantiate rockpaperscissors chaincode
 cd $GOPATH/src/github.com/njeans/fabric-test/fabric-test
+
+#copy new docker-compose file that uses named volumes instead of bind mounts
+cp /opt/docker-compose-from-docker.yaml $GOPATH/src/github.com/njeans/fabric-test/fabric-test/docker-compose-cli.yaml
+
 ./byfn.sh generate
+
+#copy releavant crypto data and binaries to named volumes 
+cp $GOPATH/src/github.com/njeans/fabric-test/fabric-test/channel-artifacts/genesis.block /opt/crypto/orderer.example.com/orderer.genesis.block
+cp -r $GOPATH/src/github.com/njeans/fabric-test/fabric-test/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/* /opt/crypto/orderer.example.com/
+cp -r $GOPATH/src/github.com/njeans/fabric-test/fabric-test/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/* /opt/crypto/peer0.org1.example.com/
+cp -r $GOPATH/src/github.com/njeans/fabric-test/fabric-test/crypto-config/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/* /opt/crypto/peer1.org1.example.com/
+cp -r $GOPATH/src/github.com/njeans/fabric-test/fabric-test/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/* /opt/crypto/peer0.org2.example.com/
+cp -r $GOPATH/src/github.com/njeans/fabric-test/fabric-test/crypto-config/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/* /opt/crypto/peer1.org2.example.com/
+cp -r $GOPATH/src/github.com/njeans/fabric-test/chaincode/rockpaperscissors/ /opt/chaincode/rockpaperscissors/
+cp -r $GOPATH/src/github.com/njeans/fabric-test/fabric-test/* /opt/crypto/cli/
+cp -r $GOPATH/src/github.com/njeans/fabric-test/fabric-test/crypto-config /opt/crypto/cli/crypto
+cp $GOPATH/src/github.com/hyperledger/fabric/.build/bin/peer /opt/peer-bin
+
+#install and instantiate rockpaperscissors chaincode
 ./byfn.sh up
